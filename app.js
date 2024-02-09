5ff5a6560f93a693026e62c942c2ebe9491d0fb6
Sivakumar <sit20cs075@sairamtap.edu.in>
2024-02-09 20:54:09 +0530

    signup page parcel

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express=require("express");
 const User = require("./connection");
 
+
+
 require("./connection");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/index.html");
@@ -15,6 +19,23 @@ app.get('/login',(req,res)=>{
 
 app.get('/signup',(req,res)=>{
     res.sendFile(__dirname+"/signup.html");
+})
+ 
+
+app.post('/signup',async (req,res)=>{
+    const name =req.body.name;
+    const password = req.body.password;
+    console.log(name,password);
+    try {
+       const user =await User.create({name,password});
+       if(user){
+        res.status(200).send("user parcel");
+
+       } 
+    } catch (err) {
+        console.log(err);
+        
+    }
 })
 
 
